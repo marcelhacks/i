3bfb23a1b2cb0916a52dbe2f64418ce7d1a6ad8d
Marcel Karlsson <100008570+marcelhacks@users.noreply.github.com>
2026-09-08 20:24:28 +0200

    Import console2 in Conduit role-collapse test

--- research/kaia-role-collapse/test/KaiaRoleCollapseConduit.t.sol
+++ research/kaia-role-collapse/test/KaiaRoleCollapseConduit.t.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
+import {console2} from "forge-std/console2.sol";
 import {KaiaRoleCollapseTest, OrderComponents, Order} from "./KaiaRoleCollapse.t.sol";
 
 contract KaiaRoleCollapseConduitTest is KaiaRoleCollapseTest {
